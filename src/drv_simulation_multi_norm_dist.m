131b6f787ce095d770c16208ce85b6e85e58c034
Yaojin Sun <sunyaojin2011@foxmail.com>
2019-02-14 23:45:15 -0500

    add optimal smoothed covariance generator

--- src/drv_simulation_multi_norm_dist.m
+++ src/drv_simulation_multi_norm_dist.m
@@ -11,7 +11,7 @@ close all
 % -------------- Basic Parameters
 p = 100;
 n = p * 0.2;
-Mu = ones(p,1);
+Mu = zeros(p,1);
 SigmaMatrix = zeros(p, p);
 varrho = 0.4;
 
@@ -23,7 +23,10 @@ end
 
 X = genrndmvnorm(n, p, Mu', SigmaMatrix);
 
+S = get_S(X);
 
 [beta_O_Ell, alpha_O_Ell] = get_Ell_beta_alpha(X)
+
+S_alpha_beta_Ell = beta_O_Ell * S + alpha_O_Ell * eye(p);
 
 
