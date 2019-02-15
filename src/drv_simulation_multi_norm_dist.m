22440433fb33546272faf743d80e8a168bab10b3
ysun52 <sunyaojin2011@foxmail.com>
2019-02-15 14:05:13 -0500

    figure added

--- src/drv_simulation_multi_norm_dist.m
+++ src/drv_simulation_multi_norm_dist.m
@@ -10,23 +10,21 @@ close all
 
 % -------------- Basic Parameters
 p = 100;
-n = p * 0.2;
-Mu = zeros(p,1);
-SigmaMatrix = zeros(p, p);
+
+c_inv = [0.2:0.03:1.2];
+NMSE_list = zeros(length(c_inv),1);
 varrho = 0.4;
 
-for i = 1:p
-    for j = 1:p
-        SigmaMatrix(i, j) = varrho^(abs(i-j));
-    end
+for i=1:length(c_inv)
+    n = floor(p*c_inv(i));
+    NMSE_list(i,1) = simulate_mv_t_dist(p, n, varrho);
 end
 
-X = genrndmvnorm(n, p, Mu', SigmaMatrix);
 
-S = get_S(X);
-
-[beta_O_Ell, alpha_O_Ell] = get_Ell_beta_alpha(X)
-
-S_alpha_beta_Ell = beta_O_Ell * S + alpha_O_Ell * eye(p);
+figure
+plot(c_inv, NMSE_list, 'r-o','linewidth',2)
+xlabel('c^{-1}')
+ylabel('NMSE')
+title('Given \rho=0.4')
 
 
---
+++ src/simulate_mv_t_dist.m
@@ -0,0 +1,29 @@
+function [NMSE] = simulate_mv_t_dist(p, n, varrho)
+%
+%  p:  number of variables
+%  n:  number of observations
+
+
+
+Mu = zeros(p,1);
+SigmaMatrix = zeros(p, p);
+
+for i = 1:p
+    for j = 1:p
+        SigmaMatrix(i, j) = varrho^(abs(i-j));
+    end
+end
+
+X = genrndmvnorm(n, p, Mu', SigmaMatrix);
+S = get_S(X);
+[beta_O_Ell, alpha_O_Ell] = get_Ell_beta_alpha(X);
+S_alpha_beta_Ell = beta_O_Ell * S + alpha_O_Ell * eye(p);
+
+ NMSE = (norm(SigmaMatrix-S_alpha_beta_Ell, 'fro'))^2/(norm(SigmaMatrix, 'fro')^2);
+
+
+
+
+
+end
+
